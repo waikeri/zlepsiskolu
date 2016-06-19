d1bdd86fb597e170af0d51c35ce353dc72241b4e
Vit Ulicny <vit.ulicny@gmail.com>
2016-06-19 02:07:19 +0200

    assets

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    //$('.nav .nav-item a').click(function (e) {
+    //    e.preventDefault()
+    //    $(this).tab('show');
+    //})
+    var owl =  $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            },
+            //1200: {
+            //    items: 5
+            //}
+        }
+    });
+    $('#carousel-next').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    $('#carousel-prev').click(function() {
+        owl.trigger('prev.owl.carousel');
+    })
+    $('.owl-carousel .item h4').fontFlex(18, 36, 50);
+});
